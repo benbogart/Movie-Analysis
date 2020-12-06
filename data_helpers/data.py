a061cb87ee0f73c1aff6f39dc26d3ef443b6d7fe
Ben Bogart <ben@benbogart.com>
2020-12-06 16:06:55 -0500

    clean up repo

--- data_helpers/data.py
+++ data_helpers/data.py
@@ -11,9 +11,9 @@ def get_movie_data():
         filename_df = pd.read_csv(filename, index_col=0)
         csv_files_dict[filename_cleaned] = filename_df
 
-    csv_files_dict['rt_movie_info'] = pd.read_csv('data/zippedData/rt.movie_info.tsv.gz', sep='\t', index_col='id')
-    csv_files_dict['rt_reviews'] = pd.read_csv('data/zippedData/rt.reviews.tsv.gz', sep='\t', encoding='latin-1')
-    
+    # csv_files_dict['rt_movie_info'] = pd.read_csv('data/zippedData/rt.movie_info.tsv.gz', sep='\t', index_col='id')
+    # csv_files_dict['rt_reviews'] = pd.read_csv('data/zippedData/rt.reviews.tsv.gz', sep='\t', encoding='latin-1')
+
     return csv_files_dict
 
 data = get_movie_data()
