018aec1182383c4966a47ad99102d4803e397044
Ben Bogart <ben@benbogart.com>
2020-12-06 14:19:44 -0500

    All elements complete

--- package_test/__init__.py
+++
@@ -1,1 +0,0 @@
-myvar = "This is a var set at import of package_test"
--- package_test/mymod.py
+++
@@ -1,4 +0,0 @@
-mymdvar = "This is a var set in the mymod module of the package_test package"
-
-def mymdfunc():
-    return "This is my function"
